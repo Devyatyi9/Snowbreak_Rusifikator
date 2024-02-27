fa84bfbf1d6667527e85295fd0140288b5b84c31
Devyatyi9 <devyatyi9@gmail.com>
2024-02-27 13:40:06 +0400

    token udpate

--- Models/MainModel.cs
+++ Models/MainModel.cs
@@ -113,7 +113,7 @@ namespace Snowbreak_Rusifikator.Models
 
             // Проверка обновлений  // установка в \game\Game\Content\Paks\~mods
             HttpClient client = UseHttpClient(Client);
-            List<RepositoryFile> repositoryFiles = await ProcessRepositoriesAsync(client, repoLink);
+            List<RepositoryFile> repositoryFiles = await ProcessRepositoriesAsync(client, repoLink, ProgramVariables.gitHubToken);
             // Сортировка
             repositoryFiles = SortingRepositoryFiles(repositoryFiles);
             if (repositoryFiles.Count == 0) { throw new Exception("FileList Empty!"); } // сделать выдачу статуса "Репозиторий пуст" и завершать функцию
@@ -240,7 +240,7 @@ namespace Snowbreak_Rusifikator.Models
         }
 
         // https://learn.microsoft.com/dotnet/fundamentals/networking/http/httpclient-guidelines#recommended-use
-        static async Task<List<RepositoryFile>> ProcessRepositoriesAsync(HttpClient client, string urlLink)
+        static async Task<List<RepositoryFile>> ProcessRepositoriesAsync(HttpClient client, string urlLink, string token)
         {
             // добавить проверку на наличие интернет соединения, и завершать функцию со статусом "Нет соединения"
             var options = new JsonSerializerOptions
@@ -255,7 +255,7 @@ namespace Snowbreak_Rusifikator.Models
             }
             if (repositoryFiles == null || repositoryFiles.Count == 0)
             {
-                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", parameter: token);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", parameter: token);
                 using HttpResponseMessage secondResponse = await client.GetAsync(urlLink);
                 repositoryFiles = await JsonSerializer.DeserializeAsync<List<RepositoryFile>>(secondResponse.Content.ReadAsStream(), options);
             }
